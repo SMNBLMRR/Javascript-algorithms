261b3c3c987f03adf29c76a78c5eab7698a56bfd
Simone Marrandino <simoneblog9991@gmail.com>
2024-01-05 12:06:27 +0100

    bubble sort

---
+++ bubblesort.js
@@ -0,0 +1,20 @@
+const array = Array.from({length:20},() => Math.floor(Math.random() * 100));
+console.log(array);
+
+function bubbleSort(arr){
+    let isChange = false;
+    while(!isChange){
+        isChange = true;
+        for(let i = 0; i < arr.length; i++){
+            if(arr[i] > arr[i + 1]){
+                let temp = arr[i];
+                arr[i] = arr[i + 1];
+                arr[i + 1] = temp
+                isChange = false;
+            }
+        }
+    }
+    return arr;
+}
+
+console.log(bubbleSort(array))
