026a70a5a5d82372c87fc3022b523c269185aee4
Simone Marrandino <simoneblog9991@gmail.com>
2024-01-08 03:45:21 +0100

    4,5,5.2

---
+++ leetcode/5.2.js
@@ -0,0 +1,9 @@
+// Given an array nums of size n, return the majority element.
+
+// The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.
+
+var majorityElement = function(nums) {
+    let majority = Math.floor(nums.length / 2);
+    let sortedArr = nums.sort((a,b) => a - b);
+    return sortedArr[majority];
+}
---
+++ leetcode/5.js
@@ -0,0 +1,25 @@
+// Given an array nums of size n, return the majority element.
+
+// The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.
+
+//Liear solution
+
+//see 5.2 for a compact algorithms
+function findMax(obj,max){
+    return Object.keys(obj).find(k => obj[k] === max)
+}
+
+var majorityElement = function(nums) {
+    let max = -Infinity;
+    let obj = {};
+    for(let i = 0; i < nums.length; i++){
+        if (!obj[nums[i]]) {
+            obj[nums[i]] = 1;
+        } else {
+            obj[nums[i]]++
+        }
+        max = Math.max(max,obj[nums[i]])
+    } 
+    return findMax(obj,max)
+};
+
